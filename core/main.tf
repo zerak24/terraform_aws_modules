153adc0f6f4508d8756feb9d9d394a518f2e3a05
duyle <duylk@tenxtenx.com>
2025-03-14 17:34:25 +0700

    chores: update

--- core/main.tf
+++ core/main.tf
@@ -315,17 +315,17 @@ module "alb" {
       cidr_ipv4   = "0.0.0.0/0"
     }
   }
-  listeners = each.value.listeners
-  # listeners = {
-  #   ex_http = {
-  #     port     = 80
-  #     protocol = "HTTP"
+  # listeners = each.value.listeners
+  listeners = {
+    ex_http = {
+      port     = 80
+      protocol = "HTTP"
 
-  #     forward = {
-  #       target_group_key = "ex_asg"
-  #     }
-  #   }
-  # }
+      forward = {
+        target_group_key = "ex_asg"
+      }
+    }
+  }
 
   target_groups = {
     ex_asg = {
