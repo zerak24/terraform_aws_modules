5e87cadbc7f02321149ba44b6869285604375e57
duyle <duylk@tenxtenx.com>
2025-03-16 22:02:27 +0700

    chores: update

--- core/script/web-server-setup.sh
+++ core/script/web-server-setup.sh
@@ -3,10 +3,10 @@
 apt update -y
 apt install apache2 unzip -y
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
-
 echo '<html><body><h1>Hello World!</h1></body></html>' | sudo tee /var/www/html/index.html
 
 systemctl enable apache2
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
